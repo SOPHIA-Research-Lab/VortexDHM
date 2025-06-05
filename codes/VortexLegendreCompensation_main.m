4bf1f4523394a9882124b0434e898461abd46738
KOrtega-S <gaezna@hotmail.com>
2025-06-04 18:22:13 -0600

    VortexLegendreCompensation_main.m

--- codes/VortexLegendreCompensation_main.m
+++ codes/VortexLegendreCompensation_main.m
@@ -1,20 +1,22 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Title:-->        VortexLegendreCompensation_main                                  
-%                                                                                                               
-%                                                                                  
-% Description:-->  This algorithm allows to evaluate the performance of phase      
-%                  compensation from two steps: 1) Solving the Vortex subpixel 
-%                  frequencies and 2) fitting square Legendre polynomials. 
-%                               
-%                                                                                  
+% Title:-->        VortexLegendreCompensation_main
+%
+%
+% Description:-->  This algorithm allows to evaluate the performance of phase
+%                  compensation from two steps: 1) Solving the Vortex subpixel
+%                  frequencies and 2) fitting square Legendre polynomials.
+%
+%
 % Authors:-->      Rene Restrepo(2), Karina Ortega-Sánchez(1,2), Carlos Trujillo(2)
-%                  Ana Doblas(2) Alfonso Padilla (1)
+%                  Ana Doblas(3) Alfonso Padilla (1)
 %                  (1) Universidad Politecnica de Tulancingo
-%                  (2) EAFIT Univeristy SOPHIA Research Group (Applied Optics)                                                                                                   
-% Email:-->        rrestre6@eafit.edu.co                                         
-% Date:-->         12/17/2024                                                      
-% version 1.0 (2024)                                                               
-% Notes-->                                                                         
+%                  (2) EAFIT Univeristy SOPHIA Research Group (Applied Optics)
+%                  (3) Department of Electrical and Computer Engineering, University 
+%                      of Massachusetts Dartmouth
+% Email:-->        rrestre6@eafit.edu.co
+% Date:-->         12/17/2024
+% version 1.0 (2024)
+% Notes-->
 
 
 %% clear memory, worksapce,close all figures and clear command window (memory variables)
@@ -23,58 +25,87 @@ close all% close all windows
 clear all% clear of memory all variable
 
 %% Lines to add folders for reading images and/or functions
-addpath('../holograms');
+addpath('../vortex/HoloPaper');
 
-%% Load the hologram 
-name = 'starTarget.bmp'; %StarTraget telecentric
-% name =  'usaf.bmp'; %USAF telecentric
-% name ='-4cm_20x_star.tiff'; %Star target no telecentric
-% name =  'redBlood30mm.tiff'; % Red blood cell no telecentric
+%% Load the hologram
 
-[hologram,M,N,m,n] = functions_evaluation.holo_read(name);
+name =  'NredBlood_40x_632_4.65_30mm.tiff';
+% name =  'TDrosophila_0x_632_5.33.bmp';
+% name =  'Tesper_0x_632_3.75.bmp';
+% name =  'Tglibocut_40x_532_3.45.jpg';
+% name =  'TProbiotics_20x_632_3.75.bmp';
+% name =  'Tslm1_10x40x_632_3.75.bmp';
+% name =  'NStar_20x_532_5.86_-4cm.tiff';
 
+hologram = double(imread(name));
+[~, nameNoExt] = fileparts(name);
+%% Verified that the hologram has a square size
+hologram = hologram(:,:,1);
+if size(hologram,2) ~= size(hologram,1)
+    lim=round(size(hologram,2)-size(hologram,1))/2;
+    hologram = hologram(:,lim:end-lim-1);
+end
+
+[N, M]=size(hologram);
+[n,m] = meshgrid(-M/2:M/2-1,-N/2:N/2-1);
 %% Lines to load parameters and define constants
-%Use these parameters for the starTarget.bmp and the usaf.bmp
-lambda = 0.632;
-dxy = 3.75;
+posUnderscore = strfind(nameNoExt, '_');
 
-% Use these parameters for the redBlood30mm.tiff and the -4cm_20x_star.tiff
-% lambda = 0.532;
-% dxy = 4.75;
+if length(posUnderscore) < 3
+    lambda = 0.632;
+    dxy = 3.75;
+else
+    lambda_str = (nameNoExt(posUnderscore(2) + 1 : posUnderscore(2) + 3));
+    lambda = str2double(['0.' lambda_str]);
+    dxy = str2double(nameNoExt(posUnderscore(3) + 1 : posUnderscore(3) + 4));
+end
 k = 2 * pi / lambda;
 fx_0 = M/2;
 fy_0 = N/2;
 
-%% Vortex subpixel frequencies calculation
-% Selection order (+1 DO)
+%% Algorithm parameters to change
+medFilt = 3; % Change between 3 or 1
+NoPistonCompensation = false; % "False" to enable piston compensation.
+UsePCA = true; % "True" to enable compensation using PCA.
+limit= 64/2; % Spatial frequency support region region for calculating the Legendre coefficients.
+
+%% Filter oreder +1
 [holo_filtered, fxOverMax, fyOverMax, cir_mask] = ...
-    functions_evaluation.spatial_filter(hologram,M,N,'Yes',3);
+    functions_evaluation.spatial_filter(hologram,M,N,'0',3);
+%% Vortex subpixel frequencies calculation
 
-% Tilt compensation aberration using by optical vortex
-[positions] = vortexCompensation(hologram, cir_mask, fxOverMax, fyOverMax);
+ft_holo = log(abs(fftshift(fft2(fftshift(hologram)))).^2);
+field = medfilt2(ft_holo, [medFilt, medFilt], 'symmetric');
+[positions] = vortexCompensation(field, fxOverMax, fyOverMax);
 
-%% Hologrom correction
+
+%% Tilt compensation aberration using optical vortex
+
 [ref_wave] = functions_evaluation.reference_wave...
     (M,N,m,n,lambda,dxy,positions(1),positions(2),k,fx_0,fy_0);
-
 field_compensate = ref_wave.*holo_filtered;
+
 figure,imagesc(angle(field_compensate)),colormap(gray),colorbar
-title('Phase compensated'),daspect([1 1 1])
+title('Phase compensated (Vortex)'),daspect([1 1 1])
 
-%% Square Legendre polynomials fitting
-gridSize = size(angle(field_compensate),1);
-if size(field_compensate,2) ~= size(field_compensate,1)
-    limit=round(size(field_compensate,2)-size(field_compensate,1))/2;
-    field_compensate = field_compensate(:,limit:end-limit-1);
+[phaseCorrected, Legendre_Coefficients] = ...
+    LegendreCompensation(field_compensate, limit, NoPistonCompensation, UsePCA);
+
+%% Vortex + piston
+
+if ~NoPistonCompensation
+    PistonCorrtection = ones(size(field_compensate)).*Legendre_Coefficients(1);
+    vortexPistonCorrection = field_compensate.*(exp(1i.*PistonCorrtection));
+    figure,imagesc(angle(vortexPistonCorrection)),colormap(gray),colorbar
+    title('Phase compensated (Vortex + Piston)'),daspect([1 1 1])
 end
-
+gridSize = size(angle(field_compensate),1);
 [X, Y] =  meshgrid(-1:(2 / gridSize):(1 - 2 / gridSize), ...
     -1:(2 / gridSize):(1 - 2 / gridSize));
 dA=(2 / gridSize) ^ 2;
-order = 1:5;
-[polynomials] = squareLegendrefitting(order, X, Y);
 
-% Orthonormalization of Legendre polynomials
+order = 2:6;
+[polynomials] = squareLegendrefitting(order, X, Y);
 Legendres = reshape(polynomials, [size(polynomials, 1)*size(polynomials, 2) ...
     size(polynomials, 3)]);
 zProds = Legendres.'* Legendres * dA;
@@ -82,19 +113,22 @@ zNorm = bsxfun(@rdivide, Legendres, sqrt(diag(zProds).'));
 Legendres = (bsxfun(@times, (ones(size(order))').', zNorm));
 Legendres_norm_const =sum(Legendres.^2,1)*dA;
 
-phaseVector = reshape(phase_unwrap(angle(field_compensate(1:gridSize, 1:gridSize))), ...
-    [size(polynomials, 1)*size(polynomials, 2) 1]);
+WavefrontReconstructed_Vect = sum(repmat(Legendre_Coefficients(2:size(order,2)+1)./ ...
+    sqrt(Legendres_norm_const(1:size(order,2))),[size(Legendres,1) 1]).*Legendres(:,1:size(order,2)),2);
 
-% Coefficients calculation of Legendre polynomials
-Legendre_Coefficients=sum(bsxfun(@times, Legendres, phaseVector), 1)*dA;
+WavefrontReconstructed = reshape(WavefrontReconstructed_Vect, ...
+    [size(polynomials, 1) size(polynomials, 2)]);
 
-WavefrontReconstructed_Vect = sum(repmat(Legendre_Coefficients(1:end)./ ...
-    sqrt(Legendres_norm_const(1:end)),[size(Legendres,1) 1]).*Legendres(:,1:end),2);
-WavefrontReconstructed = reshape(WavefrontReconstructed_Vect, [size(polynomials, 1) size(polynomials, 2)]);
+compensatedHologram = (abs(field_compensate).*exp(1i.* angle(field_compensate))./ ...
+            exp((1i).*WavefrontReconstructed));
 
-PhaseNoPistonAndTilts = (exp(1i.* angle(field_compensate(1:gridSize, 1:gridSize)))./ ...
-    exp((1i).*WavefrontReconstructed));
+figure,imagesc(angle(compensatedHologram)),colormap(gray),colorbar
+title('Phase compensated (Vortex + Legendre)'),daspect([1 1 1])
 
-figure(4), imagesc(angle(PhaseNoPistonAndTilts)), colorbar, colormap gray
+if ~NoPistonCompensation
+    PistonCorrtection = ones(size(compensatedHologram)).*Legendre_Coefficients(1);
+    vortexPistonCorrection = compensatedHologram.*(exp(1i.*PistonCorrtection));
+    figure,imagesc((angle(vortexPistonCorrection))),colormap(gray),colorbar
+    title('Phase compensated (Vortex + Legendre + Piston)'),daspect([1 1 1])
 
-
+end
